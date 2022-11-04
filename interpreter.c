00bf686aedb173aa938158ee74fef0503a72b422
toptobes <96998732+toptobes@users.noreply.github.com>
2022-11-04 09:42:46 -0500

    213 byte bf interpreter

--- interpreter.c
+++ interpreter.c
@@ -1,8 +1,29 @@
 #include "interpreter.h"
 
-char a[9999]={},*p=a,s,d;int i(char*c){for(;(d=*c)&&*c!=93;++c){p-=d==60;p+=d==62;*p-=d==45;*p+=d==43;d==
-46?putchar(*p):d==44?gets(p):d==91&&*p&&i(1+c--);if(*c==91&!*p)for(s=0;s-=*++c==93,*c!=93|s
-!=0;)s+=c[-1]==91;}}
+char a[9999]={},*p=a,s,d;int i(char*c){for(;(d=*c)&&d!=93;c++){p-=d==60;p+=d==62;*p-=d==45;*p+=d==43;d==
+46?putchar(*p):d==44?gets(p):d==91&&*p&&i(1+c--);if(*c==91&!*p)for(s=0;s+=*c==91,s-=*++c==93,*c!=93|s
+!=0;);}}
+
+//char a[9999] = {}, *p = a, s, d;
+//
+//int i(char *c) {
+//    for (; (d = *c) && d != 93; c++) {
+//        p -= d == 60;
+//        p += d == 62;
+//        *p -= d == 45;
+//        *p += d == 43;
+//
+//        d == 46
+//            ? putchar(*p)
+//            : d == 44
+//                ? gets(p)
+//                : d == 91 && *p &&
+//                    i(1 + c--);
+//
+//        if (*c == 91 & !*p)
+//            for (s = 0; s += *c == 91, s -= *++c == 93, *c != 93 | s!= 0;);
+//    }
+//}
 
 void reset()
 {
